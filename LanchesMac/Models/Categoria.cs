b2d2b927416151d2ce4a6fb10aa22885040d2be1
breno <brenoaraujom@outlook.com>
2022-09-27 22:38:44 -0300

    Criando String de Conexão com DB

--- LanchesMac/Models/Categoria.cs
+++ LanchesMac/Models/Categoria.cs
@@ -1,10 +1,28 @@
-﻿namespace LanchesMac.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Xunit;
+
+namespace LanchesMac.Models
 {
+    [Table("Categorias")]
     public class Categoria
     {
+        [Key]
         public int CategoriaId { get; set; }
+
+
+        [Required(ErrorMessage = "Informe o nome da categoria")]
+        [StringLength(100, ErrorMessage = "O tamanho maximo é 100 caracteres")]
+        [Display(Name = "Nome")]
         public string CategoriaNome { get; set; }
+
+
+
+        [Required(ErrorMessage = "Informe a descrição da categoria")]
+        [StringLength(200, ErrorMessage = "O tamanho maximo é 200 caracteres")]
+        [Display(Name = "Descricao")]
         public string Descricao { get; set; }
+       
 
         public List<Lanche> Lanches { get; set; }
     }
